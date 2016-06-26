25b2383a725897815daf632f41c60ad3852f9cb9
Mingyi <rvlfly@gmail.com>
2016-06-26 08:59:20 -0400

    Create a lib for utility functions

--- crypto/toy_implementations/block_cipher_mode.py
+++ crypto/toy_implementations/block_cipher_mode.py
@@ -2,6 +2,7 @@
 #
 
 from Crypto.Cipher import AES
+from UdacityCrypto import *
 
 # Remember, this is NOT secure cryptology code
 # This is for fun and education.  Unless you're planning
@@ -113,64 +114,7 @@ def test():
     cipher = cipher_block_chaining(plaintext, key, iv, 128, xor_encoder)
     assert bits_to_string(cipher) == 'C?\x17\x0f+=sb0O37/7|c\x03 @Ilkj3$%/hd9#$'
     
-###################
-# Here are some utility functions
-# that you might find useful
 
-BITS = ('0', '1')
-ASCII_BITS = 8
-
-def display_bits(b):
-    """converts list of {0, 1}* to string"""
-    return ''.join([BITS[e] for e in b])
-
-def seq_to_bits(seq):
-    return [0 if b == '0' else 1 for b in seq]
-
-def pad_bits(bits, pad):
-    """pads seq with leading 0s up to length pad"""
-    assert len(bits) <= pad
-    return [0] * (pad - len(bits)) + bits
-        
-def convert_to_bits(n):
-    """converts an integer `n` to bit array"""
-    result = []
-    if n == 0:
-        return [0]
-    while n > 0:
-        result = [(n % 2)] + result
-        n = n / 2
-    return result
-
-def string_to_bits(s):
-    def chr_to_bit(c):
-        return pad_bits(convert_to_bits(ord(c)), ASCII_BITS)
-    return [b for group in 
-            map(chr_to_bit, s)
-            for b in group]
-
-def bits_to_char(b):
-    assert len(b) == ASCII_BITS
-    value = 0
-    for e in b:
-        value = (value * 2) + e
-    return chr(value)
-
-def list_to_string(p):
-    return ''.join(p)
-
-def bits_to_string(b):
-    return ''.join([bits_to_char(b[i:i + ASCII_BITS]) 
-                    for i in range(0, len(b), ASCII_BITS)])
-
-def pad_bits_append(small, size):
-    # as mentioned in lecture, simply padding with
-    # zeros is not a robust way way of padding
-    # as there is no way of knowing the actual length
-    # of the file, but this is good enough
-    # for the purpose of this exercise
-    diff = max(0, size - len(small))
-    return small + [0] * diff
 
 
 
